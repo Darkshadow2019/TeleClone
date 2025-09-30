055851065ffeb8d4be2c4981a45eb72bdf2c06b2
Darkshadow2019 <51008944+Darkshadow2019@users.noreply.github.com>
2025-10-01 05:33:15 +0630

    Update TeleClone.ps1

--- Tools/TeleClone.ps1
+++ Tools/TeleClone.ps1
@@ -2,6 +2,9 @@
 # Version : 1.0.0.1
 # One-click setup for 5 folders (TeleClone01-05) + shortcuts
 
+# Telegram Quick Setup By D@rkshadow
+# One-click setup for 5 folders (TeleClone01-05) + shortcuts
+
 $source = "$env:APPDATA\Telegram Desktop\Telegram.exe"
 $desktop = [Environment]::GetFolderPath("Desktop")
 $appData = [Environment]::GetFolderPath("ApplicationData")
@@ -60,5 +63,4 @@ if (Test-Path $source) {
 } else {
     Write-Host "❌ Telegram not found at: $source" -ForegroundColor Red
     Write-Host "   Please make sure Telegram Desktop is installed" -ForegroundColor Yellow
-
 }
